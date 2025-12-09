4b7ae4b94525d0b920071b3791b3f518ee571162
Rituraj Singh <riturajsingh.8874@gmail.com>
2025-12-09 05:28:41 +0000

    day 2

---
+++ Basics/day2.js
@@ -0,0 +1,34 @@
+//let a=23
+//const b="abc";
+//var c=78
+//d=88
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+//console.log(d);
+
+let e="abc"
+let f=""
+let g=null
+let h
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+
+console.log(typeof(e))
+console.log(typeof(undefined))
+console.log(typeof(null))
+console.log(typeof(f))
+
+let score=33
+let score2="33"
+console.log(typeof(score))
+console.log(typeof score)
+
+let san=Number(score)
+console.log(typeof(san))
+
+let san2="33abc"
+console.log(typeof(san2))
